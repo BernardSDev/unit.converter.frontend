eb1409c909e3c3cfa0528c3cd64c520fa98178a8
Bernard Sam Apoh <sb.bernardsam.dev@gmail.com>
2025-04-24 17:04:16 +0000

    Add close button in Modal.tsx

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -28,15 +28,21 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
     if (!modalRoot) return null;
 
     return ReactDOM.createPortal(
-        <div className="fixed inset-0 bg-secondary/85 flex items-center justify-center z-50">
-            <div className="fixed h-[95svh] w-[95vw] grid place-content-center rounded-2xl overflow-hidden container bg-background shadow-2xl">
+        <div className="fixed inset-0 bg-background/80 flex items-center justify-center z-50">
+            <div className="fixed h-[80svh] w-[95vw] grid place-content-center rounded-2xl overflow-hidden container bg-background shadow-2xl">
                 <button
                     onClick={onClose}
-                    className="absolute text-red-500 top-3 right-5 text-4xl font-bold cursor-pointer"
+                    className="absolute text-danger top-3 right-5 text-4xl font-bold cursor-pointer"
                 >
                     &times;
                 </button>
                 {children}
+                <button
+                    onClick={onClose}
+                    className="absolute text-white bottom-3 left-5 text-sm px-6 py-2 bg-danger font-semibold cursor-pointer rounded-md"
+                >
+                    Close
+                </button>
             </div>
         </div>,
         modalRoot
