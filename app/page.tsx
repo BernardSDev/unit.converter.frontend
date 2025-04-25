6d0832536633846dc330770033c181976dd75100
Bernard Sam Apoh <sb.bernardsam.dev@gmail.com>
2025-04-25 09:33:16 +0000

    Refactor page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
 import Header from "@/app/components/Header";
 import Main from "@/app/components/Main";
-import {
-    ConvertGramsToKilograms,
-    ConvertKilometersToMiles,
-    ConvertCelsiusToFahrenheit, ConvertLitersToGallons
-} from "@/app/lib/data";
 import {JSX} from "react";
 
-export default async function Page() : Promise<JSX.Element> {
-    const sendGrams = await ConvertGramsToKilograms(100);
-    const sendKilometers = await ConvertKilometersToMiles(100);
-    const sendCelsius = await ConvertCelsiusToFahrenheit(1);
-    const sendLiters = await ConvertLitersToGallons(100);
-
-    console.log(sendGrams, sendKilometers, sendCelsius, sendLiters);
-
+export default function Page() : Readonly<JSX.Element> {
     return (
         <div>
             <Header />
